cbe0c0d8efdd6a0d48c54375b193349bd17c1456
HP <HP@DESKTOP-B0OK1FL>
2021-01-26 15:13:27 +0700

    Add project files.

---
+++ assignment 8 voting system/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace assignment_8_voting_system
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            vote[] votes = new vote[10];
+        }
+        static void castvote(vote[] votes)
+        {
+            
+            bool done = false;
+            while (done == false)
+            {
+                Console.WriteLine("what is your name?");
+                string votername = Console.ReadLine();
+                Console.WriteLine("type 1 for party 1, type 2 for party 2 or type 3 for party 3");
+                int partynumber = Console.Read();
+                if( partynumber == 1)
+                {
+                    partyname = "party1"
+                }
+            }
+            
+        }
+    }
+}
---
+++ assignment 8 voting system/vote.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace assignment_8_voting_system
+{
+    class vote
+    {
+        private string votername;
+        private string partyname;
+        private DateTime votetime;
+        private static int total;
+
+        public vote(string votername, string partyname)
+        {
+            this.votername = votername;
+            this.partyname = partyname;
+            votetime = DateTime.Now;
+        }
+        public string getpartyname()
+        {
+            return partyname;
+        }
+        public int gettotalvotes()
+        {
+            return total;
+        }
+
+        public DateTime gettimevoted() => votetime;
+
+    }
+}
